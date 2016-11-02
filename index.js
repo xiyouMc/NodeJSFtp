4f14dd2413bf488ce44f28522303ebbdc13caa4f
Mc <694449212@qq.com>
2016-11-02 16:36:56 +0800

    for dev.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var cp = require('child_process');
 var rootpath = 'root';
 var sv = server.create({
     port: '8888',
-    host: '127.0.0.1',
+    host: '192.168.2.40',
     root: rootpath
 });
-cp.exec('explorer http://127.0.0.1:8888', function () {
+cp.exec('explorer http://192.168.2.40:8888', function () {
 });
